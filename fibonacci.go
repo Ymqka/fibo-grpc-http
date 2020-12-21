a6318f69767a4140839e6c97105c5b3cd555fd24
osimonevin <osimonevin@gmail.com>
2020-12-21 23:29:05 +0300

    init commit

---
+++ fibonacci.go
@@ -0,0 +1,7 @@
+package main
+
+func fibonacci(start, stop uint) []uint64 {
+
+	rv := []uint64{0, 1, 1}
+	return rv
+}
---
+++ fibonacci_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fibonacci(t *testing.T) {
+	type args struct {
+		start uint
+		stop  uint
+	}
+	tests := []struct {
+		name string
+		args args
+		want []uint64
+	}{
+		{"fib start 1, stop 3", args{start: uint(1), stop: uint(3)}, []uint64{0, 1, 1}},
+		// {"fib start 10, stop 12", args{10, 12}, []uint64{34, 55, 89}},
+		// {"fib start 15, stop 20", args{15, 20}, []uint64{377, 610, 987, 1597, 2584}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fibonacci(tt.args.start, tt.args.stop); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fibonacci() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
