62074b486139dd3d3721d21401667aa08a69d173
osimonevin <osimonevin@gmail.com>
2020-12-21 23:57:37 +0300

    added very simple http fibo server

--- fibonacci.go
+++ fibonacci.go
@@ -1,6 +1,6 @@
 package main
 
-func fibonacci(start, stop uint) []uint64 {
+func fibonacci(start, stop int) []uint64 {
 
 	rv := []uint64{0, 1, 1}
 	return rv
--- main.go
+++ main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func fiboPage(w http.ResponseWriter, r *http.Request) {
+	keys := r.URL.Query()
+
+	rawStart := keys.Get("start")
+	rawStop := keys.Get("stop")
+
+	if rawStart == "" || rawStop == "" {
+		http.Error(w, "start or stop is not provided", 400)
+		return
+	}
+
+	start, _ := strconv.Atoi(rawStart)
+	stop, _ := strconv.Atoi(rawStop)
+
+	fiboSeries := fibonacci(start, stop)
+
+	fmt.Fprintln(w, fiboSeries)
+
+	return
+}
+
+func handleRequests() {
+	http.HandleFunc("/fibo", fiboPage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
 func main() {
+	handleRequests()
 }
