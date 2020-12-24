7b307f50ef8b262f4fa5933f3ec22d24279a5d2b
osimonevin <osimonevin@gmail.com>
2020-12-25 00:41:12 +0300

    add zero start handling

--- pkg/fibonacci/fibonacci_validator.go
+++ pkg/fibonacci/fibonacci_validator.go
@@ -10,6 +10,13 @@ type tooSmallNumber struct{}
 
 func (s *tooSmallNumber) Error() string { return "cannot handle number higher than 90" }
 
+type zeroStartIsNotAllowed struct {
+}
+
+func (s *zeroStartIsNotAllowed) Error() string {
+	return "start should be >= 1 (first sequence idx = 1)"
+}
+
 func validateFiboRange(start, stop uint32) error {
 	if stop < start {
 		return new(stopLesserThanStart)
@@ -17,6 +24,10 @@ func validateFiboRange(start, stop uint32) error {
 
 	if stop > 90 {
 		return new(tooSmallNumber)
+	}
+
+	if start == 0 {
+		return new(zeroStartIsNotAllowed)
 	}
 
 	return nil
