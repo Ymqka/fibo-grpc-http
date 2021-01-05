6b3ab57eea2516d252d0d901326da208a5459fdc
osimonevin <osimonevin@gmail.com>
2021-01-05 14:22:27 +0300

    make http tests works properly

--- pkg/fibonacci/fibonacci_validator.go
+++ pkg/fibonacci/fibonacci_validator.go
@@ -1,19 +1,17 @@
 package fibo
 
 // StopLesserThanStart stop < start
-type stopLesserThanStart struct{}
+type startHigherThanStop struct {
+	start, stop uint32
+}
 
-func (s *stopLesserThanStart) Error() string { return "stop is higher than start" }
+func (s *startHigherThanStop) Error() string { return "start is higher than stop" }
 
 // tooHigh prevent user asking for too big number
 type tooHigh struct{}
 
 func (s *tooHigh) Error() string {
-	return `stop is higher than 10 000, to force request use force flag
-http example: 
-curl "http://localhost:10000/fibonacci?start=10009&stop=10010&force=1"
-grpc client example:
-&pb.FiboRangeRequest{Start: 1, Stop: 10, Force: true}`
+	return "failed to make request, stop is higher than 10 000 and force flag is not used"
 }
 
 type lessThanZeroIsNotAllowed struct {
@@ -26,7 +24,7 @@ func (s *lessThanZeroIsNotAllowed) Error() string {
 func validateFiboParams(p Params) error {
 
 	if p.Stop < p.Start {
-		return new(stopLesserThanStart)
+		return new(startHigherThanStop)
 	}
 
 	if p.Stop > 10000 && p.Force == false {
